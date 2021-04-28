d2201efcde058603a18184fbe96460c8b5b56c7a
techwar007k <champkalpesh@gmail.com>
2021-04-28 12:14:32 +0300

    techwar007k: 03 practice done

---
+++ Pratice number 3/Pratice2.py
@@ -0,0 +1,72 @@
+# Ex 1.1
+
+x=0
+print(x)
+
+#Ex 1.2
+
+x = .0
+print(x)
+
+#Ex 1.3
+
+x= 101
+print(x)
+
+#Ex 1.4
+
+float(1000)
+x = 1000.000
+
+#Ex 1.5
+
+x = 1e3
+
+#Ex 1.6
+
+x = float(input("Enter x: "))
+if x > 0:
+   print("Positive number")
+elif x == 0:
+   print("Zero")
+else:
+   print("Negative number")
+
+#Ex 1.7
+
+x = 15
+y = 4
+
+
+print('x + y =',x+y)
+
+
+print('x - y =',x-y)
+
+
+print('x * y =',x*y)
+
+
+print('x / y =',x/y)
+
+
+print('x // y =',x % y)
+
+
+print('x ** y =',x**y)
+
+#Ex 1.8
+x, y = 2.3, 4.5
+
+#Ex 1.9
+
+x = (2.6+5.5)
+
+#Ex 1.10
+
+x = (2.6+5)*6
+
+#Ex 1.11
+
+res = 8 / 7
+res = float(8)%7
